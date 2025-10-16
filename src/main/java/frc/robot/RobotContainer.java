a6ad63b4c3aac9ee01f0f56893d05cd7d6837682
Joel Odom <joelodom@gmail.com>
2025-10-16 09:19:07 -0400

    Ports over controller bindings.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,66 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
+import frc.robot.localization.PoseEstimator8736;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+import frc.robot.subsystems.drivetrain.DrivetrainController;
 
 public class RobotContainer {
+  private static final double MAX_SPEED_METERS_PER_SEC = 8.0;
+  private static final double MAX_ANGULAR_RAD_PER_SEC = 3*Math.PI;
+
+  private final Drivetrain drivetrain = new Drivetrain();
+  private final PoseEstimator8736 poseEstimator = new PoseEstimator8736();
+  private final DrivetrainController drivetrainController = new DrivetrainController(poseEstimator);
+
+  private static final int CONTROLLER_PORT = 0;
+  private final CommandPS4Controller controller = new CommandPS4Controller(CONTROLLER_PORT);
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    controller.cross().onTrue(new InstantCommand(
+      () -> {
+        this.poseEstimator.zeroGyro();
+      }
+    ));
+
+    drivetrain.setDefaultCommand(
+      new RunCommand(
+          () -> {
+              double forward = -this.controller.getLeftY(); // Negative to match FRC convention
+              double strafe = -this.controller.getLeftX();
+              double rotation = -this.controller.getRightX();
+
+              // apply deadbands and scaling
+
+              final double DEADBAND = 0.04;
+              forward = Math.abs(forward) > DEADBAND ? forward : 0.0;
+              strafe = Math.abs(strafe) > DEADBAND ? strafe : 0.0;
+              rotation = Math.abs(rotation) > DEADBAND ? rotation : 0.0;
+
+              ChassisSpeeds speeds = new ChassisSpeeds(
+                  forward*forward*forward*MAX_SPEED_METERS_PER_SEC,
+                  strafe*strafe*strafe* MAX_SPEED_METERS_PER_SEC,
+                  rotation*rotation*rotation*MAX_ANGULAR_RAD_PER_SEC
+              );
+
+              // convert to robot-oriented coordinates and pass to swerve subsystem
+              ChassisSpeeds robotOriented = drivetrainController.fieldToRobotChassisSpeeds(speeds);
+              drivetrain.setDesiredState(robotOriented);
+          },
+          drivetrain
+      )
+  );
+}
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
