7dc873c21ee1b1b1a9ffeb702afd85d294772dea
Joel Odom <joelodom@gmail.com>
2025-10-16 09:08:06 -0400

    Ports over pose estimator and drivetrain controller. Untested.

---
+++ src/main/java/frc/robot/subsystems/drivetrain/DrivetrainController.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems.drivetrain;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import frc.robot.localization.PoseEstimator8736;
+
+public class DrivetrainController {
+    private final PoseEstimator8736 poseEstimator;
+
+    public DrivetrainController(PoseEstimator8736 poseEstimator) {
+        this.poseEstimator = poseEstimator;
+    }
+
+    public ChassisSpeeds fieldToRobotChassisSpeeds(ChassisSpeeds fieldOriented)
+    {
+        double angle = 2*Math.PI*poseEstimator.getYaw();
+
+        double cosA = Math.cos(angle);
+        double sinA = Math.sin(angle);
+
+        double vxRobot = fieldOriented.vxMetersPerSecond*cosA + fieldOriented.vyMetersPerSecond*sinA;
+        double vyRobot = -fieldOriented.vxMetersPerSecond*sinA + fieldOriented.vyMetersPerSecond*cosA;
+        
+        return new ChassisSpeeds(vxRobot, vyRobot, fieldOriented.omegaRadiansPerSecond);
+    }
+}
