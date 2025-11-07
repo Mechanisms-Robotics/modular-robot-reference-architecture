d70f7f0bbab184c38d442e7a805e261b37e64f7a
Joel Odom <joelodom@gmail.com>
2025-11-06 20:15:40 -0500

    Verified that the robot still drives after fixing some crashes.

--- src/main/java/frc/robot/PoseEstimator8736.java
+++ src/main/java/frc/robot/PoseEstimator8736.java
@@ -3,13 +3,14 @@ package frc.robot;
 import com.reduxrobotics.sensors.canandgyro.Canandgyro;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 
 /*
  * TEST PLAN:
  * 
- * Test fieldToRobotChassisSpeeds because of changes to the way we handle gyro angles.
+ * Determine where to initialize the pose estimator.
  * Test output of getModulePositions to make sure they are correct.
  */
 
@@ -18,22 +19,38 @@ public class PoseEstimator8736 {
     private final Canandgyro gyro = new Canandgyro(GYRO_CAN_ID);
 
     private SwerveDrivePoseEstimator poseEstimator;
+    private Drivetrain drivetrain;
 
     // TODO: Where do we call this from and do we need to make sure drivetrain encoders are set first?
     // Can we just call this whenever we call setModulesToEncoders? Should they be called together?
     public void initialize(Pose2d pose, Drivetrain drivetrain) {
         double yawInRotations = pose.getRotation().getDegrees() / 360.0; // between 0.0 and 1.0
         this.gyro.setYaw(yawInRotations);
+        this.drivetrain = drivetrain;
         this.poseEstimator = new SwerveDrivePoseEstimator(
             drivetrain.getKinematics(),
-            pose.getRotation(), // initial gyro angle
+            pose.getRotation(), // initial gyro angle  TODO: Should I use the actual gyro angle instead?
             drivetrain.getModulePositions(),
             pose);
     }
 
     public void zeroGyro() {
-        // TODO: Do we need to do anything with the poseEstimator when we zero the gyro?
+        // also resets the pose estimator, if it has been previously initialized
+        
+        Pose2d startingPose = null;
+        if (this.poseEstimator != null) {
+            startingPose = getPose();
+            startingPose = new Pose2d(
+                startingPose.getX(),
+                startingPose.getY(),
+                new Rotation2d(0.0));
+        }
+
         this.gyro.setYaw(0.0);
+
+        if (startingPose != null && this.drivetrain != null) {
+            initialize(startingPose, this.drivetrain);
+        }
     }
     
     public Pose2d getPose() {
