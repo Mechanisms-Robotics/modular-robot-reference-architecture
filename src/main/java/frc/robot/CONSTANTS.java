1378460c0efc29b7dfcabb35279b5926d0b147fd
NolanMartzall <Nolan.M@themechanisms.org>
2025-11-18 18:54:27 -0500

    Reorganize CONSTANTS class structure and comments

--- src/main/java/frc/robot/CONSTANTS.java
+++ src/main/java/frc/robot/CONSTANTS.java
@@ -5,10 +5,14 @@ import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Translation2d;
 
 public class CONSTANTS {
+    //RobotContainer
+    public static final int CONTROLLER_PORT = 0;
+    
     // Vision Constants
     public static AprilTagFieldLayout APRILTAG_FIELD_LAYOUT 
         = AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeWelded);
 
+    //Drivetrain
     public static final Translation2d FRONT_LEFT_MODULE_LOCATION 
         = new Translation2d(0.33, 0.23);
     public static final Translation2d FRONT_RIGHT_MODULE_LOCATION 
@@ -33,8 +37,7 @@ public class CONSTANTS {
     public static final int BACK_RIGHT_STEERING_CAN_ID = 3;
     public static final int BACK_RIGHT_DRIVE_CAN_ID = 7;
     public static final int BACK_RIGHT_ENCODER_CAN_ID = 3;
-    
-    public static final int CONTROLLER_PORT = 0;
+
     // TODO: This should be set to the robot's actual max speeds and then we should set the controller's
     // response curves. Determine these experimentally.
     public static final double MAX_SPEED_METERS_PER_SEC = 5.0;
@@ -47,7 +50,9 @@ public class CONSTANTS {
     public static final double STEERING_GEAR_RATIO = 150.0/7.0; // from SDS website
     public static final double DRIVE_GEAR_RATIO = 6.75; // L2 from SDS website
 
+    //Localization
     public static final int GYRO_CAN_ID = 9;
 
 }
+
 
